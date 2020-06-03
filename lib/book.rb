736a5a82df9a7464a84346a0806d1118552da6b5
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-03 05:02:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,3 +8,37 @@ def title
 end
 end
 
+def author=(author)
+  @author = author
+end
+
+def author 
+  @author
+end
+
+end
+
+def page_count=(num)
+  @page_count = num
+end
+
+def page_count
+  @page_count
+end
+
+def genre=(genre)
+  @genre = genre
+end
+
+def genre
+  @genre
+end
+
+def turn_page
+  puts "Flipping the page... wow, you read fast!"
+end
+
+
+end
+
+
